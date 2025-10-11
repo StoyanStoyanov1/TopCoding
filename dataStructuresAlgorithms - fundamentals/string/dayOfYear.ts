6a5eed763cea52eddd32a8cf93ad7231bc0c5914
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-10-11 15:10:42 +0200

    dayOfYear

---
+++ dataStructuresAlgorithms - fundamentals/string/dayOfYear.ts
@@ -0,0 +1,31 @@
+function dayOfYear(date: string): number {
+    const [year, month, day]: number[] = date.split("-").map(Number);
+
+    const daysOfMonth: Record<number, number> = {
+        1: 31,
+        2: isLeap(year) ? 29 : 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31,
+    };
+
+    let days = 0;
+
+    for (let i = 1; i < month; i++) {
+        days += daysOfMonth[i];
+    }
+
+    days += day;
+    return days;
+}
+
+function isLeap(year: number): boolean {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
