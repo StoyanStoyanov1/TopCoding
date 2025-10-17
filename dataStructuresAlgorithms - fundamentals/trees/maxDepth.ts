d04ad07cb4fcc6b12621480ea84c9233639f8509
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-10-17 17:16:48 +0200

    maxDepth

--- dataStructuresAlgorithms - fundamentals/trees/maxDepth.ts
+++ dataStructuresAlgorithms - fundamentals/trees/maxDepth.ts
@@ -4,21 +4,11 @@ interface TreeNode {
     left: TreeNode | null,
 }
 
-var maxDepth = function(root: TreeNode | null) {
+function maxDepth(root: TreeNode | null): number {
     if (!root) return 0;
 
-    if (!root?.left && !root?.right) return 1;
+    const left: number = maxDepth(root.left) + 1;
+    const right: number = maxDepth(root.right) + 1;
 
-    return getMaxDepth(root, 1, 1);
+    return Math.max(left, right);
 };
-
-function getMaxDepth(root: TreeNode, count: number, maxNum: number) {
-    if (!root.left && !root.right) return Math.max(count, maxNum);
-    
-
-    if (root.left) maxNum = getMaxDepth(root.left, count + 1, maxNum);
-
-    if (root.right) maxNum = getMaxDepth(root.right, count + 1, maxNum);
-
-    return maxNum
-}
