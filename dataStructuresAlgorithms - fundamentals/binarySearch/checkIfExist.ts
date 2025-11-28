ade2e43dcb8ddd3b71658fa755c6c21e87d783ca
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-11-28 22:31:24 +0100

    checkIfExist

---
+++ dataStructuresAlgorithms - fundamentals/binarySearch/checkIfExist.ts
@@ -0,0 +1,9 @@
+function checkIfExist(arr: number[]): boolean {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] === arr[j] * 2) return true;
+        }
+    }
+
+    return false;
+}
