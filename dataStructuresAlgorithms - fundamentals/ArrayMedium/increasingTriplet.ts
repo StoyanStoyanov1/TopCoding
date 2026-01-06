1dd4c5528085acabdd6ef675f7a324c6599644a3
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-01-06 13:28:56 +0100

    increasingTriplet

---
+++ dataStructuresAlgorithms - fundamentals/ArrayMedium/increasingTriplet.ts
@@ -0,0 +1,15 @@
+function increasingTriplet(nums: number[]): boolean {
+    const n: number = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            for (let k = j + 1; k < n; k++) {
+                if (nums[i] < nums[j] && nums[j] < nums[k]) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
