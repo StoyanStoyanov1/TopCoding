299cdf485ecb383abb75c91f3213d95f92ad28bb
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-01-29 11:49:17 +0100

    checkValidString

---
+++ dataStructuresAlgorithms - fundamentals/StringMedium/checkValidString.ts
@@ -0,0 +1,28 @@
+function checkValidString(s: string): boolean {
+    const left: number[] = [];
+    const star: number[] = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            left.push(i);
+        } else if (s[i] === "*") {
+            star.push(i);
+        } else { // ')'
+            if (left.length) {
+                left.pop();
+            } else if (star.length) {
+                star.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    while (left.length && star.length) {
+        if (left.pop()! > star.pop()!) {
+            return false;
+        }
+    }
+
+    return left.length === 0;
+}
