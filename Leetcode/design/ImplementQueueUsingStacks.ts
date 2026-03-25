f340ce3cf86412ba97351c8dcd3843f10466c079
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-03-25 13:53:52 +0100

    Implement Queue using Stacks

---
+++ Leetcode/design/ImplementQueueUsingStacks.ts
@@ -0,0 +1,37 @@
+class MyQueue {
+    s1: number[];
+    s2: number[];
+
+    constructor() {
+        this.s1 = [];
+        this.s2 = [];
+    }
+
+    push(x: number): void {
+        this.s1.push(x);
+    }
+
+    pop(): number {
+        if (this.s2.length === 0) {
+            while (this.s1.length) {
+                this.s2.push(this.s1.pop());
+            }
+        }
+
+        return this.s2.pop();
+    }
+
+    peek(): number {
+        if (this.s2.length === 0) {
+            while (this.s1.length) {
+                this.s2.push(this.s1.pop());
+            }
+        }
+
+        return this.s2[this.s2.length - 1];
+    }
+
+    empty(): boolean {
+        return !this.s1.length && !this.s2.length;
+    }
+}
